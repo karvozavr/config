74ba45c237b2f15d9ae0ff8e60599449e5c45416
Dmitrii Abramov <karvozavr@gmail.com>
2022-07-24 19:49:52 +0100

    Add ground-up installation for brew & ansible

---
+++ install-brew.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -e
+
+[ -z $(which brew) ] && /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+[ -z $(which ansible) ] && brew install ansible
+
+exit 0
